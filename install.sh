f7daa962530c695d6b811429cbff99f88ca4998c
luca@schluer.eu <luca@schluer.eu>
2025-03-13 23:50:17 +0100

    added error logs

--- install.sh
+++ install.sh
@@ -1,27 +1,36 @@
 #!/bin/bash
+# Under construction
+# This script asumes a minimal install using the arch install script
 
 REPO_URL="https://github.com/luca0s/dotfiles.git"
 REPO_NEOVIM="https://github.com/luca0s/nvim.git"
 
 # 1. Install git and yay
 echo "Installing git and yay ..."
-sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
+
+sudo pacman -S --needed git base-devel
+git clone https://aur.archlinux.org/yay.git || { echo Failed to clone yay repo; exit 1; }
+cd yay || { echo Failed to move to directory; exit 1; }
+makepkg -si --no-confirm || { echo Failed to install yay form AUR; exit 1; }
+
+yay -Syu --no-confirm || { echo Failed to upgrade system; exit 1; }
 
 # 2. Clone the dotfiles repo
 echo "Cloning dotfiles repo ..."
-git clone "$REPO_URL" "~/dotfiles"
+git clone "$REPO_URL" "~/dotfiles" || { echo Failed to clone dotfiles; exit 1; }
 
 # 3. Install packages from pkglist
 echo "Installing all the packages ..."
-yay -S < "~/dotfiles/pkg.txt"
+yay -S --no-confirm < "~/dotfiles/pkg.txt" || { echo Failed to install packages from package list; exit 1; }
 
 # 4. Download the neovim config
 echo "Installing neovim config ..."
-git clone "$REPO_NEOVIM" "~/.config/nvim"
+git clone "$REPO_NEOVIM" "~/.config/nvim" || { echo Failed to clone nvim config; echo 1; }
 
 # 5. Install rust and tms
 echo "Launching rust installer ..."
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+# TODO: still need to source cargo here such that the next command dosn't fail
 
 # 6. Install tms via cargo
 cargo install tmux-sessionizer
